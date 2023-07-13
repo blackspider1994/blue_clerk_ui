bed4544470f30dc6ed19f59688778f328a83f97e
Usama Zulfiqar <usama.sama@gmail.com>
2023-07-14 02:00:12 +0500

    fix: ts error disabled use in conditionally rendered block job type

--- src/app/modals/bc-invoice-item-modal/bc-invoice-item-modal.tsx
+++ src/app/modals/bc-invoice-item-modal/bc-invoice-item-modal.tsx
@@ -356,7 +356,7 @@ let isFixedDisabled=false;
                 classes={{ label: classes.checkboxLabel }}
                 control={
                   <Checkbox
-                    disabled={formik.values.itemType == 'Product'}
+                    // disabled={formik.values.itemType == 'Product'}
 
                     color={'primary'}
                     checked={formik.values.isJobType}
